da23512c91e31ad8577f7f32fba142b53c1926ab
IDN Maker Space <madeagusandi446@gmail.com>
2022-04-21 05:22:13 +0700

    New

---
+++ button1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Halaman uji")
+print("Hello My name is made agus")
+
+
+i = input("Masukan nilai : ")
+i = float(i)
---
+++ ujiulang.py
@@ -0,0 +1,7 @@
+import button1
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(0,4):
+    print(i)
